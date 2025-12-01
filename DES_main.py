f0175c1fc574b236703127300af94d1eb1306ad2
goudi zahran <judyzahran7@gmail.com>
2025-12-01 19:40:00 +0200

    Update DES_main.py
    debugged. error handling loop. randomized key. user inputs plaintext.

--- DES_main.py
+++ DES_main.py
@@ -2,27 +2,42 @@
 # DES Implementation
 # Goudi Zahran 2400725
 
-
 # main file 
 
 from DES_encode import des_encrypt_block
 from DES_decode import des_decrypt_block
+import random
 
 def hex64(x):
     return f"0x{x:016X}"
 
 def main():
-    # Example values (you can replace these)
-    key = 0x133457799BBCDFF1
-    plaintext = 0x0123456789ABCDEF
+    # generate a random 64-bit key
+    key = random.getrandbits(64)
+
+    # loop until valid plaintext is entered
+    while True:
+        plaintext_str = input("please enter the message you'd like to encrypt: ")
+
+        # check if the message is too long
+        if len(plaintext_str) > 16:
+            print("error. message too long. please try again.")
+            continue
+
+        try:
+            plaintext = int(plaintext_str, 16)
+            break  # valid input, exit loop
+        except ValueError:
+            print("invalid input. please try again.")
+            continue
 
     ciphertext = des_encrypt_block(plaintext, key)
     decrypted_text = des_decrypt_block(ciphertext, key)
 
-    print("key:         ", hex64(key))
-    print("plaintext:   ", hex64(plaintext))
-    print("ciphertext:  ", hex64(ciphertext))
-    print("decrypted text:   ", hex64(decrypted_text))
+    print("key:            ", hex64(key))
+    print("plaintext:      ", hex64(plaintext))
+    print("ciphertext:     ", hex64(ciphertext))
+    print("decrypted text: ", hex64(decrypted_text))
 
-if __name__ == "__DES_main__":
+if __name__ == "__main__":
     main()
